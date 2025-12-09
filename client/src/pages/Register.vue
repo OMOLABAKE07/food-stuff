65b48e32eba248238370327cd9f429d73c17ea0f
Omolabake <omolaadeoluwa0207@gmail.com>
2025-12-09 10:42:47 +0100

    second

--- client/src/pages/Register.vue
+++ client/src/pages/Register.vue
@@ -3,7 +3,7 @@
     <div class="max-w-md w-full space-y-8">
       <div>
         <h2 class="mt-6 text-center text-3xl font-extrabold text-gray-900">
-          Create your account
+          Create your accountssss
         </h2>
       </div>
       
